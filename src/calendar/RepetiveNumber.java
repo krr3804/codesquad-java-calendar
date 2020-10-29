4a1cecae3668ac8c07bbb47dd5b131775d94ffb0
seokj <seokj@DESKTOP-D3DODB7>
2020-10-29 20:14:16 +0900

    Repeating number

---
+++ src/calendar/RepetiveNumber.java
@@ -0,0 +1,23 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class RepetiveNumber {
+	public static void main (String [] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력하시오:");
+		int number = scanner.nextInt();
+			int result [] = new int [number];
+			
+			System.out.println(number + "가 " + number + "만큼 반복됩니다.");
+			
+		for(int i=0; i<result.length; i++) {
+			
+			System.out.println(number);
+			
+		}
+	}
+	}
+	
+
