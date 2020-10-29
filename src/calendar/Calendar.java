5ce1706a04cb62cf62954ec6e023bee1b845a25a
seokj <seokj@DESKTOP-D3DODB7>
2020-10-29 19:05:43 +0900

    maximum days of entered month

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,15 +1,27 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main (String [] args) {
-	System.out.println("    일      월      화      수      목      굼      토");
+/*	System.out.println("    일      월      화      수      목      굼      토");
 	System.out.println("----------------------------");
 	System.out.println("  1   2   3   4   5   6   7");
 	System.out.println("  8   9  10  11  12  13  14");
 	System.out.println(" 15  16  17  18  19  20  21");
 	System.out.println(" 22  23  24  25  26  27  28");
 	System.out.println(" 29  30  31 ");
-	}
 	
-
+	*/
+	
+	System.out.println("입력받은 달의 최대 일 수를 구하시오 :");
+	Scanner scanner = new Scanner(System.in);
+	int month = scanner.nextInt();
+	int[] maxdays = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	
+	System.out.println(month + "월달은 " + maxdays[month-1] + "일 까지 있습니다.");
+	
+	}	
+	
 }
